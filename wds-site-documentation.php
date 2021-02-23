46737e28d66a1ecf24d19a14908cdb5d27796dda
Evan Hildreth <evan.hildreth@webdevstudios.com>
2021-02-23 10:10:59 -0500

    Add plugin info

--- wds-site-documentation.php
+++ wds-site-documentation.php
@@ -1,22 +1,26 @@
 <?php
 /**
- * Plugin Name
+ * WDS Site Documentation.
  *
- * @package           PluginPackage
- * @author            Your Name
- * @copyright         2019 Your Name or Company Name
+ * @package           WebDevStudios\Documentation
+ * @author            WebDevStudios
+ * @copyright         2021 WebDevStudios
  * @license           GPL-2.0-or-later
+ * @since             1.0.0
  *
  * @wordpress-plugin
- * Plugin Name:       Plugin Name
- * Plugin URI:        https://example.com/plugin-name
+ * Plugin Name:       WDS Site Documentation
+ * Plugin URI:        https://github.com/webdevstudios/wds-site-documentation
  * Description:       Description of the plugin.
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            Your Name
- * Author URI:        https://example.com
- * Text Domain:       plugin-slug
+ * Author:            WebDevStudios
+ * Author URI:        https://webdevstudios.com
+ * Text Domain:       wds-site-documentation
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
+
+namespace WebDevStudios\Documentation;
+

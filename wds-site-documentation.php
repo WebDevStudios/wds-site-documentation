fb99729354bec429ad3453a7a1b5c638cac461d5
Evan Hildreth <me@eph.me>
2021-02-25 15:39:14 -0500

    Change both video and PDF

--- wds-site-documentation.php
+++ wds-site-documentation.php
@@ -155,7 +155,8 @@ function media_selector_print_scripts() {
 			// Uploading files
 			var file_frame;
 			var wp_media_post_id = wp.media.model.settings.post.id; // Store the old id
-			var set_to_post_id = <?php echo esc_js( $video_id ); ?>; // Set this
+			var set_to_video_id = <?php echo esc_js( $video_id ); ?>; // Set this
+			var set_to_pdf_id = <?php echo esc_js( $pdf_id ); ?>; // Set this
 
 			jQuery('#upload_video_button').on('click', function( event ){
 
@@ -164,13 +165,13 @@ function media_selector_print_scripts() {
 				// If the media frame already exists, reopen it.
 				if ( file_frame ) {
 					// Set the post ID to what we want
-					file_frame.uploader.uploader.param( 'post_id', set_to_post_id );
+					file_frame.uploader.uploader.param( 'post_id', set_to_video_id );
 					// Open frame
 					file_frame.open();
 					return;
 				} else {
 					// Set the wp.media post id so the uploader grabs the ID we want when initialised
-					wp.media.model.settings.post.id = set_to_post_id;
+					wp.media.model.settings.post.id = set_to_video_id;
 				}
 
 				// Create the media frame.
@@ -193,6 +194,51 @@ function media_selector_print_scripts() {
 					// Do something with attachment.id and/or attachment.url here
 					$( '#wds-video-name' ).text(attachment.title);
 					$( '#wds_documentation_video_id' ).val( attachment.id );
+
+					// Restore the main post ID
+					wp.media.model.settings.post.id = wp_media_post_id;
+				});
+
+				// Finally, open the modal
+				file_frame.open();
+			});
+
+			jQuery('#upload_pdf_button').on('click', function( event ){
+
+				event.preventDefault();
+
+				// If the media frame already exists, reopen it.
+				if ( file_frame ) {
+					// Set the post ID to what we want
+					file_frame.uploader.uploader.param( 'post_id', set_to_pdf_id );
+					// Open frame
+					file_frame.open();
+					return;
+				} else {
+					// Set the wp.media post id so the uploader grabs the ID we want when initialised
+					wp.media.model.settings.post.id = set_to_pdf_id;
+				}
+
+				// Create the media frame.
+				file_frame = wp.media.frames.file_frame = wp.media({
+					title: 'Select a PDF to upload',
+					button: {
+						text: 'Use this PDF',
+					},
+					library: {
+						type: 'application/pdf',
+					},
+					multiple: false	// Set to true to allow multiple files to be selected
+				});
+
+				// When an image is selected, run a callback.
+				file_frame.on( 'select', function() {
+					// We set multiple to false so only get one image from the uploader
+					attachment = file_frame.state().get('selection').first().toJSON();
+
+					// Do something with attachment.id and/or attachment.url here
+					$( '#wds-pdf-name' ).text(attachment.title);
+					$( '#wds_documentation_pdf_id' ).val( attachment.id );
 
 					// Restore the main post ID
 					wp.media.model.settings.post.id = wp_media_post_id;
